789731d92622da5ec9581d9e66bd08257ef059e8
Julia Jeng <princessmisty48@gmail.com>
2026-06-02 14:32:57 -0500

    Started adding player movement

---
+++ Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+using System;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed {get; set;} = 400;
+	
+	public Vector2 ScreenSize;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		ScreenSize = GetViewportRect.Size();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero; //(0, 0)
+		if (Input.isActionPressed("move_right")) {
+			velocity.X += 1;
+		}
+		if (Input.isActionPressed("move_left")) {
+			velocity.X -= 1;
+		}
+		if (Input.isActionPressed("move_down")) {
+			velocity.Y -= 1;
+		}
+		if (Input.isActionPressed("move_up)) {
+			velocity.Y += 1;
+		}
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (velocity.Length() > 0) {
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		}
+		else {
+			animatedSprite2D.Stop();
+		}
+	}
+}
